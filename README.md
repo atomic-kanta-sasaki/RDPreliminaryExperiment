bf10bb350f5b756052700a2039f18ce5b2d5d19d
kanta_sasaki <k_padbjw@icloud.com>
2020-06-19 23:06:49 +0900

    rssiを取得しようと思ったがうまく動かなかった

--- README.md
+++ README.md
@@ -4,3 +4,8 @@
 pythonでどんぐらいのことができるのか調べている
 ほとんど殴り書きコードのためメモがわりに使用
 dockerを使用するとPCがLinuxだと判定されてしまい, 使用したいライブラリが使用できないため今回は一旦dockerを使用しない方向で実装する
+
+dockerコンテナに入る
+```
+docker exec -i -t CONTAINER_ID /bin/bash
+```
--- server/ScanBLEDevice.py
+++ server/ScanBLEDevice.py
@@ -1,7 +1,12 @@
-from bluetooth.ble import DiscoveryService
+import rssi
 
-service = DiscoveryService()
-devices = service.discover(2)
+interface = 'wlp1s0'
+rssi_scanner = rssi.RSSI_Scan(interface)
 
-for address, name in devices.items():
-    print("name: {}, address: {}".format(name, address))
+ssids = ['dd-wrt','linksys']
+
+# sudo argument automatixally gets set for 'false', if the 'true' is not set manually.
+# python file will have to be run with sudo privileges.
+ap_info = rssi_scanner.getAPinfo(sudo=True)
+
+print(ap_info)
