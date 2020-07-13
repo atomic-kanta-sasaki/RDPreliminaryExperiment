3bca90714495830906843b4d8e07e184c75c587b
unknown <k_padbjw@icloud.com>
2020-07-14 04:29:34 +0900

    アクティブサイトのタイトルはとれてる

--- server/ChromeWindow.py
+++ server/ChromeWindow.py
@@ -1,10 +1,32 @@
-from selenium import webdriver
-import chromedriver_binary             # パスを通すためのコード
+# from selenium import webdriver
+# import chromedriver_binary             # パスを通すためのコード
+# import time
+
+
+# # time.sleep(10)
+# #ChromeDriverのパスを引数に指定しChromeを起動
+# driver = webdriver.Chrome()
+# #指定したURLに遷移
+# driver.get("https://www.google.co.jp")
+# #カレントページのURLを取得
+# cur_url = driver.current_url
+# #カレントページのURLを表示
+# print(cur_url)
+
+ # -*- coding: utf-8 -*-
+import pyautogui
+#os系
+import re
+import os
+import subprocess
+import sys
 import time
+import array
+#win32系
+import win32gui
+import win32con 
+import time
+time.sleep(2)
 
-
-time.sleep(5)
-#カレントページのURLを取得
-cur_url = driver.current_url
-#カレントページのURLを表示
-print(cur_url)
+a =win32gui.GetWindowText (win32gui.GetForegroundWindow()) 
+print(a)
