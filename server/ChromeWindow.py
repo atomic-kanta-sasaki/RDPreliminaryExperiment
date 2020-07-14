7d1401a44604c632f4700c793c939f75b079a6e2
unknown <k_padbjw@icloud.com>
2020-07-14 16:06:27 +0900

    chrome操作基礎コード作成

--- server/ChromeWindow.py
+++ server/ChromeWindow.py
@@ -1,32 +1,35 @@
-# from selenium import webdriver
-# import chromedriver_binary             # パスを通すためのコード
-# import time
+from selenium import webdriver
+import chromedriver_binary             # パスを通すためのコード
+import time
+from selenium.webdriver.chrome.options import Options
 
 
 # # time.sleep(10)
-# #ChromeDriverのパスを引数に指定しChromeを起動
+# # ChromeDriverのパスを引数に指定しChromeを起動
 # driver = webdriver.Chrome()
-# #指定したURLに遷移
+# # 指定したURLに遷移
 # driver.get("https://www.google.co.jp")
-# #カレントページのURLを取得
-# cur_url = driver.current_url
-# #カレントページのURLを表示
-# print(cur_url)
 
- # -*- coding: utf-8 -*-
-import pyautogui
-#os系
-import re
-import os
-import subprocess
-import sys
-import time
-import array
-#win32系
-import win32gui
-import win32con 
-import time
-time.sleep(2)
 
-a =win32gui.GetWindowText (win32gui.GetForegroundWindow()) 
-print(a)
+# def getCurrentUrl():
+
+#     cur_url = driver.current_url
+#     # カレントページのURLを表示
+#     print(cur_url)
+
+
+# input = input()
+# # カレントページのURLを取得
+# if input != "":
+#     print("入っては来ている")
+#     getCurrentUrl()
+# time.sleep(10)
+options = Options()
+options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
+
+driver = webdriver.Chrome(options=options)
+
+# driver.get('https://teratail.com/questions/277418')
+
+
+print(driver.current_url)
